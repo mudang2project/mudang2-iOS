b25ea87c7e14c132c028002f4d296a6fc0ef4b9e
hassung <hanul43075@gmail.com>
2022-12-19 14:48:16 +0900

    Initial Commit

---
+++ mudang2-ios/mudang2_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mudang2_iosApp.swift
+//  mudang2-ios
+//
+//  Created by ha on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct mudang2_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
